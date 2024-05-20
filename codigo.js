1970d89cbe6f4a13fbd933af40c02394a2adba2a
Kyrre1356 <jorge1356@hotmail.com>
2024-05-20 19:36:21 -0300

    Carrusel
    Agregado el Carrusel

--- codigo.js
+++ codigo.js
@@ -15,3 +15,31 @@ iva.innerHTML = neto.value * (alicuota.value/100);
 tIva.innerHTML = neto.value * (alicuota.value/100)+neto.value;
 etiqueta.append(iva)
 etiqueta1.append(tIva)
+
+let imagenes =["img/foto1.jpg","img/foto2.jpg","img/foto3.jpg"];
+let botonAtras = document.createElement("button");
+botonAtras.innerHTML = "anterior" 
+let botonAdelante = document.createElement("button");
+botonAdelante.innerHTML = "adelante"
+let servicios = document.getElementById("servicios");
+let contador = 0;
+
+document.Carrusel.src = imagenes[contador]
+servicios.append(botonAtras);
+servicios.append(botonAdelante);
+
+botonAtras.onclick = e => {
+    contador --;
+    if(contador<0){
+        contador = imagenes.length-1;
+    }    
+    document.Carrusel.src = imagenes[contador];
+}
+botonAdelante.onclick = e =>{
+    contador ++;
+    if(contador>imagenes.length-1){
+        contador = 0;
+    }
+    document.Carrusel.src = imagenes[contador];
+}
+
